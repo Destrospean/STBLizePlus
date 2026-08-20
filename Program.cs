6d10e1171dd21c2e0f1e7d42dd3a0f2ebf834d09
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-08-20 08:45:03 -0500

    Minor change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using YamlDotNet.Serialization;
 
 namespace Destrospean.STBLizePlus
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
