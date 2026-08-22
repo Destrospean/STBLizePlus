86791b10aab1ee1f0610b356c75d8ea36a725eb7
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-08-22 12:46:57 -0500

    Remove unneeded whitespace

--- STBLize+/Program.cs
+++ STBLize+/Program.cs
@@ -95,7 +95,7 @@ namespace Destrospean.STBLizePlus
                         }
                     };
                 }
-            } 
+            }
 
             public bool NoUnhashed = false,
             UnhashedOnly = false,
